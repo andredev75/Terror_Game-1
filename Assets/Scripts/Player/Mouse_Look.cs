79aa31830978e75e0f5a730006f42958412115c3
skbruno <bruno.rodriguesc9@gmail.com>
2021-08-29 03:31:44 -0300

    Movimentação do player e camera

---
+++ Assets/Scripts/Player/Mouse_Look.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Look : MonoBehaviour
+{
+
+    public float mouse_X;
+    public float mouse_Y;
+    public float mouse_sensitivy = 100f;
+
+    public Transform player;
+
+    private float Rotation_X = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float mouse_X = Input.GetAxis("Mouse X") * mouse_sensitivy * Time.deltaTime;
+         float mouse_Y = Input.GetAxis("Mouse Y") * mouse_sensitivy * Time.deltaTime;
+
+        Rotation_X -= mouse_Y;
+        Rotation_X = Mathf.Clamp(Rotation_X, -90f, 90f);
+        
+         
+        transform.localRotation = Quaternion.Euler(Rotation_X, 0f, 0f);
+        player.Rotate(Vector3.up,mouse_X);
+
+    }
+}
---
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    [Header("Movimentação Player")]
+    [SerializeField]
+    private CharacterController controller;
+    private Vector3 move;
+
+    private float p_X;
+    private float p_Z;
+
+    [SerializeField]
+    private float jump_Force = 7f;
+
+
+    [Header("Gravidade e checar chão Player")]
+
+    [SerializeField]
+    private float gravity = -9;
+    private Vector3 velocity;
+    [SerializeField]
+    private Transform ground_check;
+    [SerializeField]
+    private float groud_Distance = 0.4f;
+    [SerializeField]
+    private LayerMask ground_Mask;
+    private bool isground;
+
+
+    [Header("Correr e agachar Player")]
+    [SerializeField]
+    private float speed_current;
+    [SerializeField]
+    private float speed_Walk = 7f;
+    [SerializeField]
+    private float speed_crouched = 3f;
+    [SerializeField]
+    private float speed_speed = 10f;
+    [SerializeField]
+    private float heigh;
+    private bool isspeed;
+    private bool iscrouched;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        heigh = controller.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ground_Check();
+
+
+
+    }
+
+    void FixedUpdate() 
+    {
+        
+        move_Player();
+        Jump ();
+        Speed_crouched ();
+
+    }
+    void move_Player ()
+    {
+        p_X = Input.GetAxis("Horizontal");
+        p_Z = Input.GetAxis ("Vertical");
+
+        move = transform.right *  p_X +  transform.forward * p_Z;
+
+        controller.Move (move * speed_current * Time.deltaTime);
+    }
+
+    void Ground_Check ()
+    {
+
+        isground = Physics.CheckSphere(ground_check.position,groud_Distance,ground_Mask);
+        controller.Move (move * speed_current * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+    void Jump ()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt (jump_Force * -2f * gravity);
+        }
+
+    }
+
+    void Speed_crouched ()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && isground && iscrouched == false)
+        {
+            speed_current = speed_speed;
+            isspeed = true;
+        }
+        else 
+        {
+            speed_current = speed_Walk;
+            isspeed = false;
+        }
+
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            iscrouched = true;
+            heigh = 2.7f;
+            speed_current = speed_crouched;
+
+        }
+        else if (isspeed == false)
+        {
+            iscrouched = false;
+            heigh = 3.8f;
+            speed_current = speed_Walk;
+
+        }
+
+        controller.height  = Mathf.Lerp (controller.height, heigh,  3.5f * Time.deltaTime);
+
+    }
+
+    
+}
