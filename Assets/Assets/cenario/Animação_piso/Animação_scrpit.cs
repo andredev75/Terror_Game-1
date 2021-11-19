2818761209e1a5d73272e8ba71efd450e8ba00b9
skbruno <bruno.rodriguesc9@gmail.com>
2021-11-18 23:00:09 -0300

    Update - audio e animação

---
+++ Assets/Assets/cenario/Animação_piso/Animação_scrpit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animação_scrpit : MonoBehaviour
+{
+    public Animator controle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controle = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("tocou");
+        controle.SetInteger("acao", 1);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("saindo");
+        controle.SetInteger("acao", 2);
+    }
+}
